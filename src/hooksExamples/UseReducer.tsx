c1cadd1cd6b0f78bc4affc9ab0f65e91f08dada1
phanendraguptha <www.phani122@gmail.com>
2025-03-22 08:10:25 -0500

    added useReducer

---
+++ src/hooksExamples/UseReducer.tsx
@@ -0,0 +1,54 @@
+// 📄 Read Docs
+// useReducer - https://react.dev/reference/react/useReducer
+// Definition - The `useReducer` hook in React is used to manage complex state logic in your components.
+//              It takes a reducer function and an initial state, and returns the current state
+//              and a dispatch function for updating the state.
+// Rules of Hooks - https://react.dev/reference/rules/rules-of-hooks
+
+import { useReducer } from "react";
+
+type ActionType =
+  | { type: "increment" }
+  | { type: "decrement" }
+  | { type: "reset" };
+type StateType = { count: number };
+
+// reducer function is used to update the state
+// it takes the current state and an action, and returns the new state
+const reducer = (state: StateType, action: ActionType) => {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    case "reset":
+      return { count: 0 };
+    default:
+      return state;
+  }
+};
+
+const UseReducer = () => {
+  // useReducer takes a reducer function and an initial state, and
+  // returns the current state and a dispatch function for updating the state.
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen w-full space-y-4">
+      <div className="flex items-center gap-2">
+        {/* dispatch function is used to update the state */}
+        <button onClick={() => dispatch({ type: "increment" })}>
+          Increment
+        </button>
+        {/* state is the current state */}
+        {state.count}
+        <button onClick={() => dispatch({ type: "decrement" })}>
+          Decrement
+        </button>
+      </div>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+    </div>
+  );
+};
+
+export default UseReducer;
